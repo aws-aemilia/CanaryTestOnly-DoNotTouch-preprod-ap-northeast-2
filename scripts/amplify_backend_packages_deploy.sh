70e4d8a2f9e1928b063d19d189f20137a3e1d2b2
David Behroozi <behroozi@amazon.com>
2020-07-23 20:57:28 +0000

    Update setup and deploy scripts to automatically configure username/aws account.  Add yes/no prompt before running each command and detect whether you are on AL2 before trying to do anything with AemiliaEdgeLambda
    cr https://code.amazon.com/reviews/CR-30216854

--- scripts/amplify_backend_packages_deploy.sh
+++ scripts/amplify_backend_packages_deploy.sh
@@ -11,7 +11,16 @@ NC='\033[0m' # No Color
 # $2 = Workspace name:
 # $3 = Package name: 
 function deploy_local_package() {
-    echo -e echo -e "${GREEN}SAM deploy $3: BEGIN${NC}"
+    
+    echo -e "${GREEN}SAM deploy $3: BEGIN${NC}"
+    while true; do
+    read -p "Do you wish to deploy this package? (y/n): " yn
+    case $yn in
+        [Yy] ) break;;
+        [Nn] ) return;;
+        * ) echo "Please answer y/n.";;
+    esac
+    done
 
     # Navigate into the directory to build
     cd "$2/src/$3"
@@ -24,26 +33,34 @@ function deploy_local_package() {
 
 
 # Setup
-echo -e "${GREEN}Ref: https://w.amazon.com/bin/view/AWS/Mobile/AppHub/Internal/DevelopmentRunbook/#HUsingdeployscript${NC}"
-echo -e "${YELLOW}This tool assumes you are in the root 'AMPLIFY' directory created by 'amplify_backend_packages_setup.sh'.${NC}"
-echo -e "${YELLOW}If you haven't run that tool - run it first and cd into the 'AMPLIFY' directory.${NC}"
-set -x #echo commands
+if [[ `pwd` != */AMPLIFY ]]; then
+ echo -e "${GREEN}Ref: https://w.amazon.com/bin/view/AWS/Mobile/AppHub/Internal/DevelopmentRunbook/#HUsingdeployscript${NC}"
+ echo -e "${YELLOW}This tool assumes you are in the root 'AMPLIFY' directory created by 'amplify_backend_packages_setup.sh'.${NC}"
+ echo -e "${YELLOW}If you haven't run that tool - run it first and cd into the 'AMPLIFY' directory.${NC}"
+ exit;
+fi
+
+#set -x #echo commands
 
 ##### Deployment order matters ##### -->  do the SAM (local) deployment
 
-# Step 1: Sem-Automated Deploy this first
-#deploy_local_package "Deploy webhook processor: BEGIN" "AemiliaWebhookProcessorLambda" "AemiliaWebhookProcessorLambda"
-#deploy_local_package "Deploy dynamodb stream: BEGIN" "AemiliaDynamoDBStreamLambda" "AemiliaDynamoDBStreamLambda"
-#deploy_local_package "Deploy control plane: BEGIN" "AemiliaControlPlaneLambda" "AemiliaControlPlaneLambda"
-#deploy_local_package "Deploy workers lambda: BEGIN" "AemiliaWorkersLambda" "AemiliaWorkersLambda"
-#deploy_local_package "Deploy warming pool: BEGIN" "AemiliaWarmingPoolInfrastructure" "AemiliaWarmingPool"
+# Step 1: Semi-Automated Deploy this first
+deploy_local_package "Deploy webhook processor: BEGIN" "AemiliaWebhookProcessorLambda" "AemiliaWebhookProcessorLambda"
+deploy_local_package "Deploy dynamodb stream: BEGIN" "AemiliaDynamoDBStreamLambda" "AemiliaDynamoDBStreamLambda"
+deploy_local_package "Deploy control plane: BEGIN" "AemiliaControlPlaneLambda" "AemiliaControlPlaneLambda"
+deploy_local_package "Deploy workers lambda: BEGIN" "AemiliaWorkersLambda" "AemiliaWorkersLambda"
+deploy_local_package "Deploy warming pool: BEGIN" "AemiliaWarmingPoolInfrastructure" "AemiliaWarmingPool"
 
 # Step 2: Manual
-#deploy_local_package "Deploy edge lambda: BEGIN" "AemiliaEdgeLambda" "AemiliaEdgeLambda" # Maybe one day 🙄
+if [[ `uname -r` != *"amzn2"* ]]; then
+  echo -e "${RED}Since you aren't on AL2 if you want to deploy AemiliaEdgeLambda deploy it manually before proceeding or <Ctrl-C>.${NC}"
+else
+  deploy_local_package "Deploy edge lambda: BEGIN" "AemiliaEdgeLambda" "AemiliaEdgeLambda"
+fi
 
 # Step 3: Semi-Automated Deploy this last
-#deploy_local_package "Deploy pioneer execute: BEGIN" "AWSMobilePioneerExecute" "AWSMobilePioneerExecute"
-#deploy_local_package "Deploy container lambda: BEGIN" "AemiliaContainerLambda" "AemiliaContainerLambda"
-#deploy_local_package "Deploy containers: BEGIN" "AemiliaContainers" "AemiliaContainer"
+deploy_local_package "Deploy pioneer execute: BEGIN" "AWSMobilePioneerExecute" "AWSMobilePioneerExecute"
+deploy_local_package "Deploy container lambda: BEGIN" "AemiliaContainerLambda" "AemiliaContainerLambda"
+deploy_local_package "Deploy containers: BEGIN" "AemiliaContainers" "AemiliaContainer"
 
 echo -e "${GREEN}ALL ITEMS COMPLETE. SCRIPT END.${NC}"
