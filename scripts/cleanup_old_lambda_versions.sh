37d7334b81967bc7ec1badc15058b3ee08f6290a
Don Kahn <donkahn@amazon.com>
2020-11-09 19:43:07 +0000

    commit 9bddcdcabe596942c6c9ccd690a6b3f5f83f31b5 Author: Don Kahn <donkahn@amazon.com> Date: 2020-11-05T22:11:36.000Z
    Remove no-prompt version, too scary
    
    commit 1011fa919fad7be52c894583da8895ec54edf6c3
    Author: Don Kahn <donkahn@amazon.com>
    Date: 2020-11-05T22:07:24.000Z
    
    Skip edge lambda v2 functions, fix bug, remove unnecessary comment
    
    cr https://code.amazon.com/reviews/CR-38008089
    
    commit e2585a203689589d306c546c9cf48c54bc4b1e7a
    Author: Don Kahn <donkahn@amazon.com>
    Date: 2020-11-05T19:41:00.000Z
    
    recomment out delete call
    
    commit d3a43a77872ce1ce1430ad1adcfee013d64bead8
    Author: Don Kahn <donkahn@amazon.com>
    Date: 2020-10-29T03:12:47.000Z
    
    cleanup script versions - with and without prompt

--- scripts/cleanup_old_lambda_versions.sh
+++ scripts/cleanup_old_lambda_versions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Iterates over all functions and delete all versions of each function before $VERIONS_TO_KEEP
+# Iterates over all functions and provide the option to delete all versions of each function before $VERIONS_TO_KEEP
 # this is a modified version of https://code.amazon.com/packages/MoroccoServiceUsefulScripts/blobs/mainline/--/GarbageCollection/cleanup_old_lambda_versions.sh
 
 # Arguments:
@@ -36,8 +36,14 @@ delete_lambdas() {
   for lambda in ${lambdas[@]}; do
     echo "===="
     echo "Lambda: ${lambda}"
-    delete_old_versions $lambda
-    echo "Done deleting old versions of lambda function: ${lambda}"
+    # Never delete any versions of edge lambda v2
+    # https://w.amazon.com/bin/view/AWS/Mobile/AppHub/Internal/Operations/Runbook/AemiliaEdgeLambdaDeployer/#HCleanUpDeployer
+    if echo $lambda | grep -q AemiliaEdgeLambdaClone; then
+        echo "Edge Lambda V2 function, skipping"
+    else
+      delete_old_versions $lambda
+      echo "Done deleting old versions of lambda function: ${lambda}"
+    fi
   done
   get_code_storage
 }
@@ -54,32 +60,42 @@ delete_old_versions() {
   echo "Number of verisons: $number_of_versions"
   [[ $number_of_versions -le $VERSIONS_TO_KEEP ]] && echo "No excess versions to delete, skipping" && return
 
-  highest_version=$(aws lambda list-versions-by-function --function-name $1 --query "Versions[?!ends_with(FunctionArn, \`LATEST\`)].FunctionArn"  | jq -r '[.[] | match("\\d+$") | .string | tonumber] | sort | .[-1]')
+  highest_version=$(aws lambda list-versions-by-function --region ${AWS_REGION} --function-name $1 --query "Versions[?!ends_with(FunctionArn, \`LATEST\`)].FunctionArn"  | jq -r '[.[] | match("\\d+$") | .string | tonumber] | sort | .[-1]')
   newest_version_to_delete=$((highest_version - $VERSIONS_TO_KEEP))
   echo "versions:"
   echo "${version_arns}"
   echo "versions to keep:"
   echo $(seq $((newest_version_to_delete + 1)) $highest_version)
-  echo "Deleting all versions up to and including version $newest_version_to_delete"
-  
-  aliases=$(aws --region ${AWS_REGION} lambda list-aliases --no-paginate --function-name $1)
-  for version_arn in ${version_arns[@]}; do
-    version_num=$(echo $version_arn | grep -Eo "[0-9]+$")
-    # WARNING - gaps in function versions may not be taken into account
-    # for example, if the function has only versions 1, 2, 3, and 7, then versions 2 and 3 will be deleted if $VERSIONS_TO_KEEP is set to 3
-    if [[ $version_num -le $newest_version_to_delete ]]
-    then
-      if echo $aliases | grep -q \"$version_num\"
-      then
-        echo "Version ${version_num} has an alias referencing it, skipping"
-      else
-        echo "Deleting version $version_num";
-        # Commented out by default to encourage testing expected output first - uncomment when ready to delete
-        aws --region ${AWS_REGION} lambda delete-function --function-name ${version_arn}
-      fi
-    else
-      echo "Skipping version $version_num";
-    fi
+  printf "Would you like to delete all versions up to and including version $newest_version_to_delete? Make sure you're following the two-person rule if in a production environment. (y/n): "
+  read DELETE
+  while true ; do
+    case "$DELETE" in
+      y|yes)
+        aliases=$(aws --region ${AWS_REGION} lambda list-aliases --no-paginate --function-name $1)
+        for version_arn in ${version_arns[@]}; do
+          version_num=$(echo $version_arn | grep -Eo "[0-9]+$")
+          if [[ $version_num -gt $newest_version_to_delete ]]; then
+            echo "Skipping version $version_num";
+          elif echo $aliases | grep -q \"$version_num\"; then
+            echo "Version ${version_num} has an alias referencing it, skipping"
+          else
+            echo "Deleting version $version_num";
+            # Commented out by default to encourage testing expected output first - uncomment when ready to delete
+            # aws --region ${AWS_REGION} lambda delete-function --function-name ${version_arn}
+          fi
+        done
+        get_code_storage
+        break
+        ;;
+      n|no)
+        echo "Okay. Nothing deleted."
+        break
+        ;;
+      *)
+        echo "Didn't recognize your response. Please answer yes or no."
+        read -p "Do you want to delete the above listed functions? " DELETE
+        ;;
+    esac
   done
 }
 
