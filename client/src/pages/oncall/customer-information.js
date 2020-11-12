0bf81533d07c725f60e9cd6d964572709e8ccdb2
Brandon Myers <brnmye@amazon.com>
2020-11-12 00:55:13 +0000

    update should show app and branch tables

--- client/src/pages/oncall/customer-information.js
+++ client/src/pages/oncall/customer-information.js
@@ -12,7 +12,7 @@ class CustomerInformation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: {},
+            dataApp: {},
             search: '',
             loading: false,
             regions: [],
@@ -27,10 +27,12 @@ class CustomerInformation extends Component {
             search: text
         }, () => {
             if (this.state.search) {
-                this.getApiData();
+                this.getApiDataApp();
+                this.getApiDataBranch();
             } else {
                 this.setState({
-                    data: {}
+                    dataApp: {},
+                    dataBranch: {}
                 })
             }
         });
@@ -45,24 +47,46 @@ class CustomerInformation extends Component {
 //     };
 // }
 
-    async getApiData() {
+    async getApiDataApp() {
         try {
-            const response = await Ajax().fetch(`/customerinfo?stage=${this.state.stage}&region=${this.state.region}&query=${this.state.search}`);
-            const {data} = response;
-            // const jsonData = await data.json();
-            console.log(data);
-            console.log("data fetch")
+            const response = await Ajax().fetch(`/appcustomerinfo?stage=${this.state.stage}&region=${this.state.region}&query=${this.state.search}`);
+            const {dataApp} = response;
+            // const jsonData = await dataApp.json();
+            console.log(dataApp);
+            console.log("dataApp fetch")
             // const formatData = jsonData.reduce((acc, curr) => {
             //     return Object.assign(acc, curr)
             // }, {});
             this.setState({
-                data
+                dataApp
             });
         } catch (error) {
             console.log(error);
-            console.log("data fetch fail")
+            console.log("dataApp fetch fail")
             this.setState({
-                data: {}
+                dataApp: {}
+            })
+        }
+    }
+
+    async getApiDataBranch() {
+        try {
+            const response = await Ajax().fetch(`/branchcustomerinfo?stage=${this.state.stage}&region=${this.state.region}&query=${this.state.search}`);
+            const {dataBranch} = response;
+            // const jsonData = await dataBranch.json();
+            console.log(dataBranch);
+            console.log("dataBranch fetch")
+            // const formatData = jsonData.reduce((acc, curr) => {
+            //     return Object.assign(acc, curr)
+            // }, {});
+            this.setState({
+                dataBranch
+            });
+        } catch (error) {
+            console.log(error);
+            console.log("dataBranch fetch fail")
+            this.setState({
+                dataBranch: {}
             })
         }
     }
@@ -82,15 +106,15 @@ class CustomerInformation extends Component {
                     <Search searchDataChanged={this.searchDataChanged} />
                 </StageRegionSelector>
                 <h4 style={this.tagStyle}>App Table</h4>
-                <Table data={this.state.data} />
+                <Table dataApp={this.state.dataApp} />
                 <h4 style={this.tagStyle}>Branch Table</h4>
-                <Table data={this.state.data} />
+                <Table dataApp={this.state.dataBranch} />
                 <h4 style={this.tagStyle}>Job Table</h4>
-                <Table data={this.state.data} />
+                <Table dataApp={this.state.dataApp} />
                 <h4 style={this.tagStyle}>Domain Table</h4>
-                <Table data={this.state.data} />
+                <Table dataApp={this.state.dataApp} />
                 <h4 style={this.tagStyle}>Webhook Table</h4>
-                <Table data={this.state.data} />
+                <Table dataApp={this.state.dataApp} />
             </div>
         )
     }
