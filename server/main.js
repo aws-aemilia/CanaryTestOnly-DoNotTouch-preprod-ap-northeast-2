e96ac99a010f347138e28c6b83789175bbe7b219
Brandon Myers <brnmye@amazon.com>
2020-11-13 01:36:32 +0000

    update

--- server/main.js
+++ server/main.js
@@ -408,7 +408,7 @@ app.get("/customerinfoBranch", async (req, res) => {
     const { stage, region, query } = req.query;
 
     // go through each branch and retrieve data from specific appId
-    const branch =  "master";
+    const branch =  "";
 
     const params = {
         "TableName": `${stage}-${region}-Branch`,
