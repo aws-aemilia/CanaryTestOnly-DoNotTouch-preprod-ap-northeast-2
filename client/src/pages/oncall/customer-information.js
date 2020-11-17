fb83209cc6cd48f87e0f4c6e54bbb0deecb4f28e
Brandon Myers <brnmye@amazon.com>
2020-11-17 03:11:52 +0000

    testing job table

--- client/src/pages/oncall/customer-information.js
+++ client/src/pages/oncall/customer-information.js
@@ -51,13 +51,19 @@ class CustomerInformation extends Component {
             promises.push(Ajax().fetch(`/customerinfoDomain?stage=${this.state.stage}&region=${this.state.region}&query=${this.state.search}`));
             // promises.push(Ajax().fetch(`/customerinfoJob?stage=${this.state.stage}&region=${this.state.region}&branchArn=${this.state.branch}`));
             const [resultApp, resultBranch, resultDomain] = await Promise.all(promises);
-            jobPromises.push(resultBranch.map(branch => Ajax().fetch(`/customerinfoJob?stage=${this.state.stage}&region=${this.state.region}&branchArn=${branch.branchArn}`)));
-            const [jobResults] = await Promise.all(jobPromises);
+
 
             this.setState({
                 appData: resultApp.data,
                 branchData: resultBranch.data,
-                domainData: resultDomain.data,
+                domainData: resultDomain.data
+            });
+
+            jobPromises.push(this.state.branchData.map(branch => Ajax().fetch(`/customerinfoJob?stage=${this.state.stage}&region=${this.state.region}&branchArn=${branch.branchArn}`)));
+            const [jobResults] = await Promise.all(jobPromises);
+
+            
+            this.setState({
                 jobData: jobResults.data
             });
 
