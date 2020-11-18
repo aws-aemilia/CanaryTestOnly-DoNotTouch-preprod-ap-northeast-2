1aeec85fc54125b2e04c5905978bbc9b4895c939
Brandon Myers <brnmye@amazon.com>
2020-11-18 00:00:33 +0000

    job

--- client/src/components/tables/AppTable.jsx
+++ client/src/components/tables/AppTable.jsx
@@ -21,9 +21,9 @@ class AppTable extends Component {
                     </tr>
                     {
                         Object.keys(this.props.data).length ? Object.keys(this.props.data).map((key, index) => (
-                            <tr key={index}>
-                                <td>{key.appId}</td>
-                                <td>{JSON.stringify(this.props.data[key.appId])}</td>                            </tr>
+                            <tr key={"appId"}>
+                                <td>{key}</td>
+                                <td>{JSON.stringify(this.props.data[key])}</td>                            </tr>
                         )) : <tr>
                                 <td>No Data Found</td>
                                 <td></td>
