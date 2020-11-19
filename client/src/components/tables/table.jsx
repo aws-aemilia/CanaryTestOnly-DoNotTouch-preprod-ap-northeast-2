1090f0cd3633ff0b69aa3c380d5babd400e98d27
Brandon Myers <brnmye@amazon.com>
2020-11-19 23:14:37 +0000

    datetime test

--- client/src/components/tables/table.jsx
+++ client/src/components/tables/table.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './table.module.css';
+import AceEditor from 'react-ace';
 
 class Table extends Component {
     constructor(props) {
@@ -20,17 +21,21 @@ class Table extends Component {
                     </tr>
                     {
                         Object.keys(this.props.data).length ? Object.keys(this.props.data).sort().map((key, index) => {
-                            // let getDate = "";
-                            // if(key === "createTime" || key === "updateTime"){
-                            //     getDate = `${JSON.stringify(data[key])}`;
-                            // }
-                            // let dateRetrieved = new Date(`${getDate}`)
-                            // : key === "createTime" ? `${dateRetrieved}` : key === "updateTime" ? `${dateRetrieved}`
+                            let getDate = "";
+                            if(key === "createTime" || key === "updateTime"){
+                                getDate = `${JSON.stringify(data[key])}`;
+                            }
+
+                            console.log("date returned", getDate);
+                            console.log("typeof var", typeof getDate)
+                            console.log("typeof var2", typeof `${getDate}`)
+                            let dateRetrieved = new Date(`${getDate}`)
+                            console.log("date retrieved", dateRetrieved)
                             return (
                                 <tr key={index}>
                                     <td>{key}</td>
                                     <td>
-                                        {JSON.stringify(data[key]) === "0" ? "False" : JSON.stringify(data[key]) === "1" ? "True" : JSON.stringify(data[key])}
+                                        {JSON.stringify(data[key]) === "0" ? "False" : JSON.stringify(data[key]) === "1" ? "True" : key === "createTime" ? `${dateRetrieved}` : key === "updateTime" ? `${dateRetrieved}` : JSON.stringify(data[key])}
                                     </td>
                                 </tr>
                             )
