0cb96fbbf250fed8742b45680158092ed9b9407b
Brandon Myers <brnmye@amazon.com>
2020-11-23 22:43:23 +0000

    test

--- client/src/components/tables/table.jsx
+++ client/src/components/tables/table.jsx
@@ -49,8 +49,8 @@ class Table extends Component {
                             : key === "commitTime" ? `${dateRetrieved}` 
                             : key === "endTime" ? `${dateRetrieved}` 
                             : key === "startTime" ? `${dateRetrieved}`
-                            : key === "subDomainDOs" || key === "jobSteps" ? JSON.stringify(data[key[key]], undefined, 2)
-                            : key === "buildSpec" ? JSON.stringify(data[key], undefined, 2).replace(/\\n/g, '\n').replace(/\"/g, "")
+                            : key === "subDomainDOs" || key === "jobSteps" ? JSON.stringify(data[key], undefined, 2).replace(/\"/g, "").replace(/\[/g, "").replace(/\]/g, "").replace(/,/g, '\n')
+                            : key === "buildSpec" ? JSON.stringify(data[key], undefined, 2).replace(/\"/g, "").replace(/\\n/g, '\n')
                             : key === "config" ? (Object.keys(config).map((configKey, configIndex) => (<div key={configIndex} className={styles.config}><h6>{configKey}</h6><p>{typeof config[configKey] === 'object' && Object.keys(config[configKey]).length === 0 ? "" : config[configKey]}</p></div>)))
                             : typeof data[key] === 'object' && Object.keys(data[key]).length === 0 ? ""
                             : JSON.stringify(data[key], undefined, 2).replace(/\"/g, "")
