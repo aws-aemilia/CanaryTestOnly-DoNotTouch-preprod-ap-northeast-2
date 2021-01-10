93f03236b5bb605b6980c6bd728dc3a3ebbc3b07
Samuel Nimako-Mensah <snimakom@amazon.com>
2021-01-09 21:40:45 -0800

    Adding scripts to clear orphaned distributions

---
+++ node-scripts/deleteDistributions.js
@@ -0,0 +1,35 @@
+`use strict`;
+const { CloudFront } = require("@aws-sdk/client-cloudfront");
+const fs = require("fs-extra");
+const orphanedDistributionsFileName = "orphanedDistributions.json";
+
+const run = async () => {
+  const client = new CloudFront({});
+  try {
+    if (fs.existsSync(orphanedDistributionsFileName)) {
+      const orphanedDistributions = fs.readJSONSync(
+        orphanedDistributionsFileName
+      );
+
+      orphanedDistributions.forEach(async distribution => {
+
+        const getDistributionRequest = {
+            Id: distribution[0]
+          };
+          let getDistributionResult = await client.getDistribution(
+            getDistributionRequest
+          );
+        
+        const deleteDistributionRequest = {
+          Id: distribution[0],
+          IfMatch: getDistributionResult.ETag
+        };
+        await client.deleteDistribution(deleteDistributionRequest);
+
+      });
+    }
+  } catch (error) {
+    console.log("An error occurred" + error);
+  }
+};
+run();
