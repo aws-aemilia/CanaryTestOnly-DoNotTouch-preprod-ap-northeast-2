849206a4933e9e8903002eccbaf1a509c21c9406
Victor Ding <weikding@amazon.com>
2021-03-25 03:00:20 +0000

    fix nav bar

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import App from './App';
 
