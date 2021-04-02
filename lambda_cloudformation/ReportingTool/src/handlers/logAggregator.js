3f1493f731b57517868d188122c30aeeff645b21
Victor Ding <weikding@amazon.com>
2021-04-02 04:27:11 +0000

    change log format

--- lambda_cloudformation/ReportingTool/src/handlers/logAggregator.js
+++ lambda_cloudformation/ReportingTool/src/handlers/logAggregator.js
@@ -1,33 +1,36 @@
 const AWS = require('aws-sdk');
 const cloudwatchlogs = new AWS.CloudWatchLogs();
-const {getRegion} = require('../helpers/accountHelper')
-var zlib = require("zlib");
+const getRegion = require('../regionHelper')
+const { v4: uuidv4 } = require('uuid');
+const zlib = require("zlib");
 
 exports.handler = async function(input, context) {
     let logEvents = [];
+
     input.Records.forEach(function(record) {
         var payload = Buffer.from(record.kinesis.data, "base64");
-        var unzippedData = zlib.gunzipSync(payload)
-        var logs = JSON.parse(unzippedData.toString("ascii"));
+        var unZippedData = zlib.gunzipSync(payload)
+        var logs = JSON.parse(unZippedData.toString("ascii"));
+        var service = logs.logGroup
         console.log(logs)
         var account = logs.owner;
         logs.logEvents.forEach(log => logEvents.push({
-            message: JSON.stringify({log: log.message, region:getRegion(account)}),
+            message: JSON.stringify({log: log.message, region:getRegion(account), service: service}),
             timestamp: Number(log.timestamp)
         }))
     })
 
-    let logStreamName = Date.now().toString()
+    let logStreamName = uuidv4().replace(/-/g, '');
 
     var createLogStreamParams = {
-        logGroupName: "Reporting/WarmingPool",
+        logGroupName: "ServiceLogs",
         logStreamName: logStreamName 
     };
     await cloudwatchlogs.createLogStream(createLogStreamParams).promise()
 
     var putLogEventsParams = {
         logEvents: logEvents,
-        logGroupName: "Reporting/WarmingPool",
+        logGroupName: "ServiceLogs",
         logStreamName: logStreamName,
     };
     await cloudwatchlogs.putLogEvents(putLogEventsParams).promise();
