dec8ff66e24644e8fbf6cb5ffcd8e8f2f5a3de13
Victor Ding <weikding@amazon.com>
2021-05-26 19:55:07 +0000

    no-op commit

--- server/main.js
+++ server/main.js
@@ -592,7 +592,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../client/public/index.html'));
 });
 
-
 // app.listen(config.port);
 // console.log('App is listening on port ' + config.port);
 module.exports = app;
