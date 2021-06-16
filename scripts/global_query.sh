735247f15a021dccd09e83ff5795811a92609948
Don Kahn <donkahn@amazon.com>
2021-06-16 14:53:32 -0700

    global_query.sh: Add runId as param, namespace results into folders

--- scripts/global_query.sh
+++ scripts/global_query.sh
@@ -15,8 +15,10 @@ END_TIME=$(perl -MDate::Manip -e 'print UnixDate(ParseDate("'$4'"),"%s")')
 QUERY=$5
 LOG_GROUP_PREFIX=$6
 FILENAME=$7
+RUN_ID=$8
   
 export AWS_ACCESS_KEY_ID="";export AWS_SECRET_ACCESS_KEY="";export AWS_SESSION_TOKEN="";
+
 credentials=$(curl -s -S -b ~/.midway/cookie -c ~/.midway/cookie -L -X POST --header "X-Amz-Target: IsengardService.GetAssumeRoleCredentials" --header "Content-Encoding: amz-1.0" --header "Content-Type: application/json; charset=UTF-8" -d "{\"AWSAccountID\": \"$ACCOUNT_ID\", \"IAMRoleName\":\"ReadOnly\"}" https://isengard-service.amazon.com | perl -ne 'use Term::ANSIColor qw(:constants); my $line = $_; my %lookup = (sessionToken=>"AWS_SESSION_TOKEN",secretAccessKey=>"AWS_SECRET_ACCESS_KEY",accessKeyId=>"AWS_ACCESS_KEY_ID"); while (($key, $value) = each (%lookup)) {my $val = $line; die BOLD WHITE ON_RED . "Unable to get credentials did you run mwinit and do you have access to the role?\n" . RESET . RED . "$line" . RESET . "\n" if ($line=~/error/);$val =~ s/.*?\\?"$key\\?":\\?"(.*?)\\?".*$/$1/e; chomp($val); print "export $value=$val\n";}print "export AWS_DEFAULT_REGION='$REGION'\n";');$(echo $credentials);
   
 exitIfUnset "$AWS_ACCESS_KEY_ID" "Unable to get credentials"
@@ -29,7 +31,7 @@ QUERY_ID=$(aws logs start-query --log-group-name $LOG_GROUP --end-time $END_TIME
   
 exitIfUnset "$QUERY_ID" "Unable to start query"
   
-touch $FILENAME
+echo "Run Id: $RUN_ID"
 echo "Region: $REGION"
 echo "Filename: $FILENAME"
 echo "Query Id: $QUERY_ID"
@@ -43,4 +45,9 @@ while true; do
   [[ "$STATUS" == 'Scheduled' || "$STATUS" == 'Running' ]] || break
   sleep 5
 done
-aws logs get-query-results --query-id $QUERY_ID --query "results[*][*].value" --output text | tee -a $FILENAME
+
+TODAY=$(date +%Y-%m-%d)
+OUTPUT_DIR="output/$TODAY-$RUN_ID"
+mkdir -p $OUTPUT_DIR
+echo "output dir $OUTPUT_DIR"
+aws logs get-query-results --query-id $QUERY_ID --query "results[*][*].value" --output text | tee -a $OUTPUT_DIR/$FILENAME
