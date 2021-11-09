128a99a7c2aab215d4e9a0f4d52eae3b94be7403
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2021-11-09 09:08:51 -0800

    Remove rvm keyserver replacement - The build image now uses rvm.io to pull keys since the old keyserver is no longer available

--- scripts/amplify_backend_packages_setup.sh
+++ scripts/amplify_backend_packages_setup.sh
@@ -62,11 +62,6 @@ function download_and_build_package() {
         # Pipeline/Versionset is "Containers" but package is "Container"
         if [ "$2/src/$3" = "AemiliaContainers/src/AemiliaContainer" ]; then
             brazil ws --use -p "AemiliaContainerNode10"
-
-            # Modify Node10 package
-            originalText='RUN gpg --keyserver hkp:\/\/pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB \&\& \\'
-            newText='RUN curl -sSL https:\/\/rvm.io\/mpapis.asc \| gpg --import - \&\& curl -sSL https:\/\/rvm.io\/pkuczynski.asc \| gpg --import - \&\& \\'
-            sed_dash_i "s/$originalText/$newText/g" src/AemiliaContainerNode10/configuration/Dockerfile.template
             
             # Modify Container package
             originalText="033345365959"
