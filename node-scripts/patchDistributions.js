ac6cb70612432c6cd617995b76c39fe6531c4812
Don Kahn <donkahn@amazon.com>
2022-03-10 16:56:01 -0800

    Repurpose distribution patcher script to update OAI

--- node-scripts/patchDistributions.js
+++ node-scripts/patchDistributions.js
@@ -10,11 +10,17 @@ const limiterFlexible = new RateLimiterMemory({
 const limiter = new RateLimiterQueue(limiterFlexible);
 
 const run = async () => {
-  if(process.argv.length !=3 || isNaN(process.argv[2])) {
-    console.log(`Usage: node patchDistributions.js maxVersion`);
-    return;
-  }
-  let maxVersion = Number(process.argv[2]);
+  // if(process.argv.length !=3 || isNaN(process.argv[2])) {
+  //   console.log(`Usage: node patchDistributions.js maxVersion`);
+  //   return;
+  // }
+  // let maxVersion = Number(process.argv[2]);
+  const OAI_PREFIX = 'origin-access-identity/cloudfront';
+  //TEST EM7BM2RKELW1H
+  const OAI_TO_ROTATE = `${OAI_PREFIX}/E2491UISJ9XA03`; 
+  
+  //TEST E2491UISJ9XA03
+  const OAI_TARGET = `${OAI_PREFIX}/EM7BM2RKELW1H`;
   const client = new CloudFront();
     if (fs.existsSync(patchDistributionsFileName)) {
       const patchDistributions = fs.readJSONSync(
@@ -22,6 +28,8 @@ const run = async () => {
       );
 
       for await (const distribution of patchDistributions) {
+        //throttle rate
+        await limiter.removeTokens(2);
         try {
           console.log(`Processing: ${distribution[0]}`);
           // Get Distribution
@@ -32,7 +40,20 @@ const run = async () => {
             getDistributionRequest
           );
 
-          let needsUpdate = true;
+          let needsUpdate = false;
+          origins = getDistributionResult.Distribution.DistributionConfig.Origins;
+          // console.log(getDistributionResult.Distribution.DistributionConfig.Origins.Items)
+          getDistributionResult.Distribution.DistributionConfig.Origins.Items.forEach(
+            (origin) => {
+              if (origin.S3OriginConfig && origin.S3OriginConfig.OriginAccessIdentity === OAI_TO_ROTATE) {
+                needsUpdate = true;
+                console.log(`Found OAI to rotate: ${OAI_TO_ROTATE} on distribution id ${getDistributionResult.Distribution.Id}, replacing it with ${OAI_TARGET}`);
+                origin.S3OriginConfig.OriginAccessIdentity = OAI_TARGET;
+              }
+            }
+          )
+
+          /*
           getDistributionResult.Distribution.DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations.Items.forEach(
             (association) => {
               //increment lambda function name, leave the rest as is.
@@ -47,7 +68,9 @@ const run = async () => {
                 needsUpdate = false;
               }
             }
-          );
+            );
+          */
+          
           if(needsUpdate) {
             let updateDistributionRequest = {
               Id: distribution[0],
@@ -56,9 +79,7 @@ const run = async () => {
               },
               IfMatch: getDistributionResult.ETag,
             };
-            
-            //throttle rate if we need to update
-            await limiter.removeTokens(1);
+
             
             //Update Distribution
             console.log(`Updating: ${distribution[0]}`);
