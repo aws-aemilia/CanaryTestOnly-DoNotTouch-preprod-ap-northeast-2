a313e15825fb95453cba604694fe58a3ff65682b
Nestor Carvantes <nestorba@amazon.com>
2022-12-19 00:51:48 -0600

    feat: add getCloudFormationResources function

--- node-scripts/utils/cloudFormation.ts
+++ node-scripts/utils/cloudFormation.ts
@@ -2,9 +2,12 @@ import {
   CloudFormationClient,
   DescribeStacksCommand,
   DescribeStacksCommandOutput,
+  ListStackResourcesCommandInput,
+  paginateListStackResources,
 } from "@aws-sdk/client-cloudformation";
 import { AmplifyAccount, getIsengardCredentialsProvider } from "../Isengard";
 import { memoizeWith } from "ramda";
+import { StackResourceSummary } from "@aws-sdk/client-cloudformation/dist-types/models/models_0";
 
 export const fetchCloudFormationOutputs = async ({
   amplifyAccount,
@@ -14,7 +17,7 @@ export const fetchCloudFormationOutputs = async ({
   amplifyAccount: AmplifyAccount;
   stackName: string;
   outputKeys: string[];
-}) => {
+}): Promise<Record<string, string>> => {
   const client = new CloudFormationClient({
     region: amplifyAccount.region,
     credentials: getIsengardCredentialsProvider(amplifyAccount.accountId),
@@ -32,6 +35,41 @@ export const fetchCloudFormationOutputs = async ({
     }, {} as Record<string, string>);
 };
 
+export const getCloudFormationResources = async ({
+  amplifyAccount,
+  stackName,
+  logicalResourceIds,
+}: {
+  amplifyAccount: AmplifyAccount;
+  stackName: string;
+  logicalResourceIds: string[];
+}): Promise<Record<string, string>> => {
+  const client = new CloudFormationClient({
+    region: amplifyAccount.region,
+    credentials: getIsengardCredentialsProvider(amplifyAccount.accountId),
+  });
+
+  const params: ListStackResourcesCommandInput = { StackName: stackName };
+
+  const resources: StackResourceSummary[] = [];
+
+  for await (const listStackResourcesCommandOutput of paginateListStackResources(
+    { client },
+    params
+  )) {
+    resources.push(
+      ...listStackResourcesCommandOutput.StackResourceSummaries!.filter((x) =>
+        logicalResourceIds.includes(x.LogicalResourceId!)
+      )
+    );
+  }
+
+  return resources.reduce((acc, resource) => {
+    acc[resource.LogicalResourceId!] = resource.PhysicalResourceId!;
+    return acc;
+  }, {} as Record<string, string>);
+};
+
 /**
  * Fetch outputs from a given CFN stack
  * @param options
@@ -40,6 +78,7 @@ export const fetchCloudFormationOutputs = async ({
  * @param options.outputKeys The CFN output keys to fetch
  */
 export const getCloudFormationOutputs = memoizeWith(
-  ({ amplifyAccount, stackName, outputKeys }) => `${amplifyAccount.accountId}|${stackName}|${outputKeys.join('#')}`,
+  ({ amplifyAccount, stackName, outputKeys }) =>
+    `${amplifyAccount.accountId}|${stackName}|${outputKeys.join("#")}`,
   fetchCloudFormationOutputs
 );
