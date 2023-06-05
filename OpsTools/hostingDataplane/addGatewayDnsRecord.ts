da6be01a28bc3376a8b1f5caf90e6ff3e271aa0c
Nestor Carvantes <nestorba@amazon.com>
2023-06-05 16:26:31 -0700

    chore: move node_scripts content to top level

--- OpsTools/hostingDataplane/addGatewayDnsRecord.ts
+++ OpsTools/hostingDataplane/addGatewayDnsRecord.ts
@@ -2,7 +2,7 @@ import { dataPlaneAccount, Region, Stage } from "../../Isengard";
 import yargs from "yargs";
 import { getCloudFormationOutputs } from "../../utils/cloudFormation";
 import { getDomainName, HOSTED_ZONE_ID } from "./utils/utils";
-import { getRoute53Client, updateRecordsInHostedZone } from "./../../route53";
+import { getRoute53Client, updateRecordsInHostedZone } from "../../route53";
 import { ChangeBatch } from "aws-sdk/clients/route53";
 
 require("util").inspect.defaultOptions.depth = null;
