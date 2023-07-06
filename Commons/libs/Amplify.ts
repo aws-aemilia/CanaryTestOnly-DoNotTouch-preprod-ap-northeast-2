9b6156ade6a1f199806099f6425c72529463174d
Nestor Carvantes <nestorba@amazon.com>
2023-07-06 10:20:25 -0700

    fix: fix Amplify.ts DynamoDBDocumentClient usage
    cr: https://code.amazon.com/reviews/CR-95890364

--- Commons/libs/Amplify.ts
+++ Commons/libs/Amplify.ts
@@ -1,5 +1,4 @@
-import { BatchGetItemCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, BatchGetCommand } from "@aws-sdk/lib-dynamodb";
 import { AppDO } from "../dynamodb";
 
 export async function getAppsByAppIds(
@@ -11,12 +10,12 @@ export async function getAppsByAppIds(
   const appTableName = `${stage}-${region}-App`;
 
   const result = await dynamodb.send(
-    new BatchGetItemCommand({
+    new BatchGetCommand({
       RequestItems: {
         [appTableName]: {
           Keys: [
             ...appIds.map((appId) => ({
-              appId: { S: appId },
+              appId: appId
             })),
           ],
         },
