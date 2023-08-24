8ecc08e50f72ec5a219a6c8c0e81253134cb315d
Peter Lisy <plisy@amazon.com>
2023-08-24 12:27:16 -0700

    fix: globalQuery uses preflightCAZ and writes results to tmp
    cr: https://code.amazon.com/reviews/CR-99854928

--- OpsTools/globalQuery.ts
+++ OpsTools/globalQuery.ts
@@ -1,13 +1,9 @@
 import { existsSync, mkdirSync, writeFileSync } from "fs";
-import { readFile } from 'fs/promises';
-import { createSpinningLogger } from "../Commons/utils/logger"
+import { readFile } from "fs/promises";
+import { createSpinningLogger } from "../Commons/utils/logger";
 import path from "path";
 import yargs from "yargs";
-import {
-  controlPlaneAccounts,
-  getIsengardCredentialsProvider,
-  StandardRoles,
-} from "../Commons/Isengard";
+import { controlPlaneAccounts, getIsengardCredentialsProvider, preflightCAZ, Stage, StandardRoles } from "../Commons/Isengard";
 import { insightsQuery } from "../Commons/libs/CloudWatch";
 import { CloudWatchLogsClient } from "@aws-sdk/client-cloudwatch-logs";
 
@@ -24,7 +20,6 @@ async function main() {
         --logGroupPrefix AWSCodeBuild \
         --startDate '2023-04-02T00:00:00-00:00' \
         --endDate '2023-04-08T00:00:00-00:00' \
-        --ticket V123456789 \
         --query 'fields @timestamp, @message, @logStream | filter strcontains(@message, "Node version not available")'
 
       Usage with query file:
@@ -33,9 +28,8 @@ async function main() {
         --logGroupPrefix AWSCodeBuild \
         --startDate '2023-04-02T00:00:00-00:00' \
         --endDate '2023-04-08T00:00:00-00:00' \
-        --ticket V123456789 \
         --queryFile ./query.txt
-      `,
+      `
     )
     .option("stage", {
       describe: "stage to run the command",
@@ -44,8 +38,7 @@ async function main() {
       choices: ["beta", "gamma", "preprod", "prod"],
     })
     .option("logGroupPrefix", {
-      describe:
-        "Prefix for the CW log group, for example /aws/lambda/AemiliaControlPlane-Function-",
+      describe: "Prefix for the CW log group, for example /aws/lambda/AemiliaControlPlane-Function-",
       type: "string",
       demandOption: true,
     })
@@ -60,8 +53,7 @@ async function main() {
       demandOption: false,
     })
     .option("startDate", {
-      describe:
-        "Query start date in ISO format, for example 2022-04-01T00:00:00",
+      describe: "Query start date in ISO format, for example 2022-04-01T00:00:00",
       type: "string",
       demandOption: true,
     })
@@ -71,16 +63,15 @@ async function main() {
       demandOption: true,
     })
     .option("outputDir", {
-      describe:
-        "Folder where to write the CSV files that contain query results",
+      describe: "Folder where to write the CSV files that contain query results",
       type: "string",
-      default: "globalQueryOutput",
       demandOption: false,
     })
-    .option("ticket", {
-      describe:
-        "SIM ticket for FullReadOnly role usage. Providing a ticket will switch to FullReadOnly role instead of ReadOnly.",
+    .option("role", {
+      describe: "IAM Role for Query",
       type: "string",
+      default: StandardRoles.ReadOnly,
+      choices: [StandardRoles.ReadOnly, StandardRoles.FullReadOnly],
       demandOption: false,
     })
     .option("noEmptyFiles", {
@@ -93,17 +84,7 @@ async function main() {
     .version(false)
     .help().argv;
 
-  const {
-    stage,
-    logGroupPrefix,
-    query,
-    queryFile,
-    startDate,
-    endDate,
-    outputDir,
-    ticket,
-    noEmptyFiles,
-  } = args;
+  const { stage, logGroupPrefix, query, queryFile, startDate, endDate, outputDir, noEmptyFiles, role } = args;
 
   if (!query && !queryFile) {
     throw new Error("Either query or queryFile must be provided");
@@ -114,67 +95,54 @@ async function main() {
     queryToExecute = await readFile(path.join(__dirname, queryFile), "utf-8");
   }
 
-  let role = StandardRoles.ReadOnly;
-  if (ticket) {
-    process.env.ISENGARD_SIM = ticket;
-    role = StandardRoles.FullReadOnly;
-  }
+  const resolveOutputFolder = outputDir ?? path.join(__dirname, "..", "tmp", `query_results_${Date.now()}}`);
 
-  if (!existsSync(outputDir)) {
+  if (!existsSync(resolveOutputFolder)) {
     logger.info("Output directory does not exist, creating it");
-    mkdirSync(outputDir, { recursive: true });
+    mkdirSync(resolveOutputFolder, { recursive: true });
     logger.info(outputDir, "Output directory created successfully");
   }
 
-  const controlPlaneAccountsForStage = (await controlPlaneAccounts()).filter(
-    (acc) => acc.stage === stage
-  );
+  const controlPlaneAccountsForStage =  await controlPlaneAccounts({stage: stage as Stage});
+
+  await preflightCAZ({
+    accounts: controlPlaneAccountsForStage,
+    role
+  })
 
   let regionsLeftToGetLogsFrom = controlPlaneAccountsForStage.length;
   logger.update(`Fetching global query results. Regions remaining: ${regionsLeftToGetLogsFrom}`);
   logger.spinnerStart();
-  const queryPromises = controlPlaneAccountsForStage.map(
-    async (controlPlaneAccount) => {
-      logger.info(controlPlaneAccount, "Beginning query for region");
-      const cloudwatchClient = new CloudWatchLogsClient({
-        region: controlPlaneAccount.region,
-        credentials: getIsengardCredentialsProvider(
-          controlPlaneAccount.accountId,
-          "FullReadOnly"
-        ),
-      });
+  const queryPromises = controlPlaneAccountsForStage.map(async (controlPlaneAccount) => {
+    logger.info(controlPlaneAccount, "Beginning query for region");
+    const cloudwatchClient = new CloudWatchLogsClient({
+      region: controlPlaneAccount.region,
+      credentials: getIsengardCredentialsProvider(controlPlaneAccount.accountId, role),
+    });
 
-      const logs = await insightsQuery(
-        cloudwatchClient,
-        logGroupPrefix,
-        queryToExecute,
-        new Date(startDate),
-        new Date(endDate),
-        logger
-      );
+    const logs = await insightsQuery(cloudwatchClient, logGroupPrefix, queryToExecute, new Date(startDate), new Date(endDate), logger);
 
-      const fileName = controlPlaneAccount.region.concat(".json");
-      const outputFile = path.join(outputDir, fileName);
+    const fileName = controlPlaneAccount.region.concat(".json");
+    const outputFile = path.join(resolveOutputFolder, fileName);
 
-      regionsLeftToGetLogsFrom--;
-      logger.update(`Fetching global query results. Regions remaining: ${regionsLeftToGetLogsFrom}`);
+    regionsLeftToGetLogsFrom--;
+    logger.update(`Fetching global query results. Regions remaining: ${regionsLeftToGetLogsFrom}`);
 
-      if (logs.length === 0) {
-        logger.info(`No results found for ${controlPlaneAccount.region}`);
-        if (noEmptyFiles) {
-          return;
-        }
+    if (logs.length === 0) {
+      logger.info(`No results found for ${controlPlaneAccount.region}`);
+      if (noEmptyFiles) {
+        return;
       }
-
-      logger.info({ outputFile }, "Writing results to file");
-      await writeFileSync(outputFile, JSON.stringify(logs, null, 2));
     }
-  );
+
+    logger.info({ outputFile }, "Writing results to file");
+    await writeFileSync(outputFile, JSON.stringify(logs, null, 2));
+  });
 
   try {
     await Promise.all(queryPromises);
     logger.spinnerStop("Completed global query");
-  } catch(error) {
+  } catch (error) {
     logger.error(error, "Failed to execute global query");
     logger.spinnerStop("Failed global query", false);
   }
