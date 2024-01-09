9c6c340b493d4cb89e48ae1de7e821077ba49f16
Saadat Azim <saazim@amazon.com>
2024-01-09 22:25:49 +0000

    feat(LEC migrator): allow executing for multiple regions concurrently
    cr: https://code.amazon.com/reviews/CR-110612028

--- Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts
+++ Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts
@@ -1,6 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { AwsCredentialIdentity, Provider } from "@aws-sdk/types";
+import { RegionName } from "Commons/Isengard/types";
+import logger from "Commons/utils/logger";
 import { toRegionName } from "Commons/utils/regions";
 import fs from "fs";
 import path from "path";
@@ -13,14 +15,13 @@ import {
   lookupCustomerAccountId,
 } from "../../Commons/dynamodb";
 import {
-  controlPlaneAccount,
+  controlPlaneAccounts,
   getIsengardCredentialsProvider,
   preflightCAZ,
   Region,
   Stage,
   StandardRoles,
 } from "../../Commons/Isengard";
-import logger from "Commons/utils/logger";
 
 const main = async () => {
   const args = await yargs(process.argv.slice(2))
@@ -34,6 +35,16 @@ npx ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=test --r
 # Testing in beta account
 npx ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=beta --region=us-west-2 --operation=MIGRATE --appOrDomainId=d36z6gc724kpno
 npx ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=beta --region=us-west-2 --operation=VERIFY
+
+# Wave 3 Prod
+ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=prod --region="eu-west-1" --region="us-east-1" --region="ap-southeast-1" --operation=MIGRATE
+
+# Wave 4 Prod
+ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --operation=MIGRATE --stage=prod --region="ap-south-1" --region="eu-central-1" --region="ap-northeast-2" --region="ap-northeast-1" --region="us-west-2" --region="ap-southeast-2"
+
+# Wave 5 Prod
+ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --operation=MIGRATE --stage=prod --region="eu-north-1" --region="me-south-1" --region="eu-west-3" --region="sa-east-1" --region="ap-east-1" --region="eu-west-2" --region="eu-south-1" --region="us-west-1"
+
     `
     )
     .option("stage", {
@@ -44,6 +55,7 @@ npx ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=beta --r
     })
     .option("region", {
       describe: "i.e. us-west-2",
+      array: true,
       type: "string",
       demandOption: true,
     })
@@ -60,47 +72,59 @@ npx ts-node Etc/TableMigrations/LambdaEdgeConfigAddAccountId.ts --stage=beta --r
     .help().argv;
 
   const { stage, region, operation, appOrDomainId } = args;
-  const regionName = toRegionName(region);
   const stageName = stage as Stage;
 
   const runId = new Date().getTime().toString();
 
-  let controlPlaneAccountId: string | undefined;
+  const accounts = await getAccounts(stageName, region);
 
-  if (stage !== "test") {
-    const account = await controlPlaneAccount(stageName, regionName);
-    await preflightCAZ({
-      accounts: account,
-      role: StandardRoles.OncallOperator,
-    });
-    controlPlaneAccountId = account.accountId;
-  }
-
-  const migrator = new LambdaEdgeConfigAccountIdMigrator(
-    stageName,
-    regionName,
-    `${operation}-${runId}`,
-    controlPlaneAccountId
+  const migrators = accounts.map(
+    (a) =>
+      new LambdaEdgeConfigAccountIdMigrator(
+        stageName,
+        a.region as Region,
+        `${operation}-${runId}`,
+        a.accountId
+      )
   );
 
-  switch (operation) {
-    case "MIGRATE":
-      if (appOrDomainId) {
-        await migrator.migrateSingleDocument(appOrDomainId);
-      } else {
-        await migrator.migrateAll();
-      }
-      break;
-    case "VERIFY":
-      await migrator.verifyAll();
-      break;
-    case "REVERT":
-      await migrator.revertAll();
-      break;
-    default:
-      throw new Error(`Unknown operation ${operation}`);
-  }
+  let operations: Promise<void>[] = [];
+
+  migrators.forEach((migrator) => {
+    switch (operation) {
+      case "MIGRATE":
+        if (appOrDomainId) {
+          operations.push(migrator.migrateSingleDocument(appOrDomainId));
+        } else {
+          operations.push(migrator.migrateAll());
+        }
+        break;
+      case "VERIFY":
+        operations.push(migrator.verifyAll());
+        break;
+      case "REVERT":
+        operations.push(migrator.revertAll());
+        break;
+      default:
+        throw new Error(`Unknown operation ${operation}`);
+    }
+  });
+
+  await Promise.all(operations);
 };
+
+async function getAccounts(stage: Stage, regions: string[]) {
+  const regionNames = regions.map((r) => toRegionName(r));
+
+  const accounts = (await controlPlaneAccounts({ stage })).filter((a) =>
+    regionNames.includes(a.region as RegionName)
+  );
+  await preflightCAZ({
+    accounts,
+    role: StandardRoles.OncallOperator,
+  });
+  return accounts;
+}
 
 const attributesToGet: (keyof LambdaEdgeConfig)[] = ["appId", "accountId"];
 type FetchedLec = Pick<LambdaEdgeConfig, "appId" | "accountId">;
@@ -164,7 +188,7 @@ class LambdaEdgeConfigAccountIdMigrator {
 
   public async migrateAll() {
     let count = 0;
-    for await (let page of await this.lecDAO.paginate(attributesToGet, 100)) {
+    for await (let page of await this.lecDAO.paginate(attributesToGet, 1000)) {
       const lecDOs = page.Items as FetchedLec[];
       if (!lecDOs) {
         console.info("No more items found in the page");
@@ -175,7 +199,7 @@ class LambdaEdgeConfigAccountIdMigrator {
       for (let lecDO of lecDOs) {
         await this.migrate(lecDO);
       }
-      logger.info(`Processed ${count} items`);
+      logger.info(`${this.stage}-${this.region} Processed ${count} items`);
     }
   }
 
@@ -216,8 +240,11 @@ class LambdaEdgeConfigAccountIdMigrator {
         }
       }
       count += appDOs.length;
-      logger.info(`Processed ${count} items`);
+      logger.info(`${this.stage}-${this.region} ${count} items processed`);
     }
+    logger.info(
+      `${this.stage}-${this.region} Completed. ${count} items verified`
+    );
   }
 
   private async verifyLec(appOrDomainId: string, exprectedAccountId: string) {
@@ -280,6 +307,11 @@ class LambdaEdgeConfigAccountIdMigrator {
 
     if (lecDO.accountId) {
       this.logToFile("MIGRATE", appOrDomainId, lecDO.accountId);
+      return;
+    }
+
+    if (appOrDomainId.includes("_")) {
+      this.logToFile("MIGRATE", appOrDomainId, "INVALID_APPID");
       return;
     }
 
