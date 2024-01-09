44b02664a480ce4035058ba7225fd93c00f5452a
Eddy Varela <eddvar@amazon.com>
2024-01-09 19:24:54 +0000

    chore: update cleanup github webhooks to use amplify-tools isengard accounts and oncall operator role
    cr: https://code.amazon.com/reviews/CR-110595599

--- Dev/cleanupGitHubWebhooks.ts
+++ Dev/cleanupGitHubWebhooks.ts
@@ -1,145 +1,142 @@
 import yargs from "yargs";
+
+import {
+  Stage,
+  StandardRoles,
+  getIsengardCredentialsProvider,
+} from "Commons/Isengard";
+import logger from "Commons/utils/logger";
+
+import { toRegionName, toAirportCode } from "Commons/utils/regions";
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from "@aws-sdk/client-secrets-manager";
 import { Octokit } from "@octokit/rest";
 
-type Webhook = {
-  id: number;
-  config: {
-    url: string;
-  };
-};
-
-const getGitAccountOwner = async (gitClient: Octokit): Promise<string> => {
-  const result = await gitClient.rest.users.getAuthenticated();
-  return result.data.login;
-};
-
-const listWebhooks = async (
-  gitClient: Octokit,
-  owner: string,
-  repo: string
-): Promise<Webhook[]> => {
-  const webhooks = await gitClient.request("GET /repos/{owner}/{repo}/hooks", {
-    owner: owner,
-    repo: repo,
-  });
-  const hooks: Webhook[] = [];
-  webhooks.data.forEach((webhook) => {
-    if (webhook.config.url) {
-      hooks.push({
-        id: webhook.id,
-        config: {
-          url: webhook.config.url,
-        },
-      });
-    }
-  });
-  return hooks;
-};
-
-const deleteWebhook = async (
-  gitClient: Octokit,
-  owner: string,
-  repo: string,
-  webhook: Webhook
-) => {
-  console.log(`deleteing webhook: ${webhook.config.url}`);
-  await gitClient.request("DELETE /repos/{owner}/{repo}/hooks/{hook_id}", {
-    owner: owner,
-    repo: repo,
-    hook_id: webhook.id,
-  });
-};
-
-const filterOutNonAmplifyWebhooks = (webhooks: Webhook[]) => {
-  return webhooks.filter((webhook) => {
-    if (webhook.config.url.match(/github\?appid=/)) {
-      return true;
-    }
-  });
-};
-
-const filterAmplifyWebhooksByRegion = (webhooks: Webhook[], region: string) => {
-  return webhooks.filter((webhook) => {
-    if (webhook.config.url.match(region)) {
-      return true;
-    }
-  });
-};
-
-const main = async () => {
+async function main() {
   const args = await yargs(process.argv.slice(2))
     .usage(
-      `
-This tool allows you to cleanup your GitHub Webhooks for a repo. Useful for when you hit the 20 webhook limit.
-**Pre-Req** You need to create a GitHub personal access token with Read/Delete permission on Webhooks.
+      `Clean up webhooks for Integ Tests per stage and region
 
-# deletes all Amplify webhooks for repo "my_repo_name"
-npx ts-node cleanupGitHubWebhooks.ts --githubToken ghp_****** --repo my_repo_name
+      Usage:
 
-# deletes all Amplify webhooks created from region "us-west-2" for repo "my_repo_name"
-npx ts-node cleanupGitHubWebhooks.ts --githubToken ghp_****** --repo my_repo_name --region us-west-2
+      npx ts-node Dev/cleanupGitHubWebhooks.ts --stage beta --region pdx
+      npx ts-node Dev/cleanupGitHubWebhooks.ts --stage gamma --region pdx
+      npx ts-node Dev/cleanupGitHubWebhooks.ts --stage gamma --region iad
       `
     )
-    .option("githubToken", {
-      describe:
-        "token for your github account. must have at least delete repo permissions",
+    .option("stage", {
+      describe: "stage to run the command",
       type: "string",
-      demandOption: true,
-    })
-    .option("repo", {
-      describe: "repo to delete webhooks for",
-      type: "string",
-      demandOption: true,
+      default: "prod",
+      choices: ["test", "beta", "gamma", "preprod", "prod"],
     })
     .option("region", {
       describe: "region to run the command",
+      demandOption: true,
       type: "string",
+    })
+    .option("dryRun", {
+      describe: "When true - doesn't actually delete the webhooks.",
+      type: "boolean",
     })
     .strict()
     .version(false)
     .help().argv;
-  const { githubToken, region, repo } = args;
-  console.log(`args: ${JSON.stringify(args)}`);
 
-  if (!githubToken) {
-    throw new Error("no github token was provided.");
-  }
+  const stage = args.stage as Stage;
+  const region = toRegionName(args.region);
+  const airportCode = toAirportCode(args.region);
+  const { dryRun } = args;
 
-  const octokit = new Octokit({ auth: githubToken });
-  const owner = await getGitAccountOwner(octokit);
-  const webhooks = await listWebhooks(octokit, owner, repo);
-  const amplifyWebhooks = filterOutNonAmplifyWebhooks(webhooks);
-  if (amplifyWebhooks.length === 0) {
-    console.log(`there are no amplify webhooks for repo ${repo}`);
-    return;
-  }
+  logger.info(
+    `Cleaning up webhooks for Integ Test Repos for ${stage} ${region} (${airportCode})`
+  );
 
-  if (!region) {
-    // delete all amplify webhooks on repo
-    for (const webhook of amplifyWebhooks) {
-      await deleteWebhook(octokit, owner, repo, webhook);
-    }
-  } else if (region) {
-    // delete all amplify webhooks for region on repo
-    const amplifyRegionWebhooks = filterAmplifyWebhooksByRegion(
-      amplifyWebhooks,
-      region
-    );
-    if (amplifyRegionWebhooks.length === 0) {
-      console.log(
-        `there are no amplify webhooks for repo ${repo} and region: ${region}`
-      );
-      return;
-    }
-    for (const webhook of amplifyRegionWebhooks) {
-      await deleteWebhook(octokit, owner, repo, webhook);
-    }
-  }
-};
+  const credentials = getIsengardCredentialsProvider(
+    "850295347350",
+    StandardRoles.OncallOperator
+  );
 
-main()
-  .then()
-  .catch((e) => {
-    console.error("\nSomething went wrong");
-    console.error(e);
+  const secretsManager = new SecretsManagerClient({
+    region: "us-east-1",
+    credentials,
   });
+
+  const { SecretString } = await secretsManager.send(
+    new GetSecretValueCommand({
+      SecretId: "IntegrationTestRepoAccessTokens",
+    })
+  );
+
+  const tokens: Record<string, string> = JSON.parse(SecretString ?? "");
+
+  const tokenKey = `github:${region}`;
+  if (!tokens[tokenKey]) {
+    throw new Error(`Could not get token for ${tokenKey}.`);
+  }
+
+  const githubToken = tokens[tokenKey];
+  logger.info("Got gh tokens");
+  const octokit = new Octokit({
+    auth: githubToken,
+  });
+
+  const owner = `aws-aemilia-${airportCode.toLowerCase()}`;
+
+  const repos = await octokit.paginate(octokit.repos.listForUser as any, {
+    username: owner,
+  });
+
+  for (const repo of repos) {
+    if (!repo.name.endsWith(`${stage}-${region}`)) {
+      continue;
+    }
+
+    const webhooks = await listInactiveRepoWebhooks(octokit, owner, repo.name);
+    logger.info({ repo: repo.name, webhooks: webhooks.length }, "Found repo");
+
+    for (const webhook of webhooks) {
+      logger.info({ repo: repo.name, webhook: webhook.id }, "Deleting webhook");
+      if (!dryRun) {
+        await octokit.repos.deleteWebhook({
+          owner,
+          repo: repo.name,
+          hook_id: webhook.id,
+        });
+        logger.info(
+          { repo: repo.name, webhook: webhook.id },
+          "Deleted webhook"
+        );
+      }
+    }
+  }
+}
+
+async function listInactiveRepoWebhooks(
+  octokit: Octokit,
+  owner: string,
+  repo: string
+) {
+  const { data } = await octokit.repos.listWebhooks({
+    owner,
+    repo,
+  });
+
+  let webhooks: typeof data = [];
+  for (const webhook of data) {
+    // It's not an Amplify webhook - skip
+    if (!webhook.config.url?.match(/github\?appid=/)) {
+      continue;
+    }
+    webhooks.push(webhook);
+  }
+
+  return webhooks;
+}
+
+main().catch((err) => {
+  logger.error(err, "Command execution failed");
+  process.exit(1);
+});
